c7b13a9b13f809f5a336f07ef15599fb368afb13
Gabriel Pimentel <gabriel.pimentel@somoshbi.com.br>
2025-03-14 14:38:36 -0300

    Template do projeto

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+    white: '#fff',
+    blue: '#3294F8',
+    red: '#E25858',
+    
+    'blue-dark': '#1E6F9F',
+    'gray-100': '#E7EDF4',
+    'gray-200': '#D9D9D9',
+    'gray-300': '#808080',
+    'gray-400': '#333333',
+    'gray-500': '#262626',
+    'gray-600': '#1A1A1A',
+    'gray-700': '#0D0D0D'
+
+} as const
