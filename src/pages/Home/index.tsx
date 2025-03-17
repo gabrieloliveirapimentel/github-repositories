62db9c76fa173a6f55f56fbdd889b22db2a59ce5
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-03-17 10:20:29 -0300

    Implementando barra de pesquisa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Card } from "../../components/Card"
 import { Profile } from "../../components/Profile"
 import { Search } from "../../components/Search"
@@ -8,6 +8,13 @@ import { IRepository } from "../../@types/types"
 
 export function Home() {
     const { profile, repositories } = useContext(ProfileContext)
+    const [ filteredRepositories, setFilteredRepositories ] = useState<IRepository[]>(repositories)
+    const [ search, setSearch ] = useState<string>('')
+
+    useEffect(() => {
+        setFilteredRepositories(repositories.filter(repository => repository.name.toLowerCase().includes(search.toLowerCase())))
+    }
+    , [search, repositories])
 
     return (
         <div>
@@ -17,9 +24,9 @@ export function Home() {
                     <h2>Repositórios</h2>
                     <span>{repositories.length} repositórios</span>
                 </Content>
-                <Search />
+                <Search search={search} updateSearch={setSearch} />
                 <List>
-                    {repositories.length > 0 ? repositories.map(( repository: IRepository )  => (
+                    {filteredRepositories.length > 0 ? filteredRepositories.map(( repository: IRepository )  => (
                         <Card key={repository.id} repository={repository} />
                     )) : (
                         <span>Nenhum repositório encontrado</span>
